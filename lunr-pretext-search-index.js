58f5e22730ab91b5bc9bb4172a54cda214113bd5
Eddie Fuller <30291001+eddiefuller@users.noreply.github.com>
2025-07-22 23:41:04 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,230 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-section-intro",
+  "level": "1",
+  "url": "sec-section-intro.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Introduction",
+  "body": " Introduction  Text of section.  "
+},
+{
+  "id": "sec-precalcrev",
+  "level": "1",
+  "url": "sec-precalcrev.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Precalculus Review",
+  "body": " Precalculus Review   This document is intended to be a reference for precalculus topics that will be useful in this course. It is not meant to be an exhaustive overview of all topics covered in the prerequisite courses for this course.    Algebra   Polynomials  A polynomial is any expression of the form where is a nonnegative integer and each coefficient, , are real numbers.    The degree of a polynomial is the largest exponent in the polynomial    The coefficient of the highest power of is called the leading coefficient       Factoring\/Expanding  There are certain formulas that will be very useful in factoring and expanding You’ll want to keep these in mind while working with polynomials.     Difference of squares :      Sum of cubes :      Difference of cubes :      Perfect square trinomial :                       Functions  A function , , is a rule that assigns a single output, , for each input, . There are four possible ways to represent a function:     Verbally – by a description in words     Numerically – by a table, diagram, or set of ordered pairs of values     Graphically – by a visual representation on a set of axes     Algebraically – by an explicit formula or equation    It is required that a function must assign a unique output to each input.   Domain and Range  The domain of a function is the set of all possible input values which yield a valid output. The range of a function is the set of all possible (valid) output values over the domain.   Representing domain and range  Domain and range are commonly represented by intervals. There are three common ways to represent intervals – interval notation, inequalities, and number lines.  Note:    The use of parentheses, open circles, or indicates that the value is not included on the interval.    The use of brackets, closed circles, or indicates that the value is included on the interval.    Open notation is always used for and .      Common domain and range rules    You cannot take the even root of a negative number.     etc. all have domains of .      You can take the odd root of any number.     etc. all have domains of .      You cannot divide by zero.     etc. all have domains of (or ).      Even powers only yield nonnegative outputs.     etc. all have ranges of .          Types of Functions   Basic functions     Linear :      Even Power : ,      Odd Power : ,      Even Root : ,      Odd Root : ,      Odd Rational : ,      Even Rational : ,      Absolute Value :       Piecewise functions  A piecewise function is a function which is defined differently for different parts of the function’s domain.  The following is an example of a piecewise function and its corresponding graph.     Composite functions  Given two functions and , the composite function , , is defined by .    Even\/Odd functions  An even function is one whose graph is symmetric about the -axis. This means that if the point is on the graph, then the point is also on the graph.    Algebraically, a function is even if the following is true for all in its domain:     An odd function is one whose graph is symmetric about the origin. This means that if the point is on the graph, then the point is also on the graph.    Algebraically, a function is odd if the following is true for all in its domain:        Transformation of Functions   Shifts in the and Directions  For and some function , the graph of is transformed by:    Vertical shift up by units:     Vertical shift down by units:     Horizontal shift to the left by units:     Horizontal shift to the right by units:       Scalings in the and Directions  For and some function , the graph of is transformed by:    Vertical stretch by a factor of :     Vertical shrink by a factor of :     Horizontal shrink by a factor of :     Horizontal stretch by a factor of :       Reflections about the and axis  For some function ,    Reflect about the axis:     Reflect about the axis:        Inverse Functions  For a one-to-one function with domain and range , there exists an inverse function of , , with domain and range such that whenever , then .    If the graph of has the point , then the graph of has the point .    A composite of the function with its inverse (or vice versa) yields the independent variable.                 NOTE: (x) is the inverse of . This is NOT the same as the reciprocal of which is .   Finding an inverse function  Suppose is a one-to-one function. In order to find :    Interchange and ( and )    Solve the resulting equation for     Let        Exponential Functions  An exponential function is a function of the form where is a real number called the base .  The natural exponential function , , has base , a non-terminating, non-repeating number ( ).  If , with , then:    The domain of is     The range of is     The graph of has a horizontal asymptote at     The graph contains the point     If , the function is always increasing    If , the function is always decreasing     Laws of Exponents  For any positive integers and and any real numbers and ,    2   ( times)   ,    ,                        Common errors using exponents:                 Logarithmic Functions  The inverse of an exponential function with base , , is a logarithmic function with base , .  The inverse of the natural exponential function, , is the natural logarithmic function , .  If ,    This is equivalent to     The domain of this function is     The range of this function is     The graph of this function has a vertical asymptote at     The graph contains the point     Since logarithmic and exponential functions are inverses of each other, then               Laws of Logarithms  For any positive numbers , and any real number ,    2                       Trigonometry   The six trigonometric functions  Let be a point on a circle of radius associated with the angle . Note that represents the length of the horizontal leg of the right triangle formed, represents the length of the vertical leg, and represents the length of the hypotenuse. Then the six trigonometric functions are defined as follows:    2                      The Unit Circle  The unit circle is a circle with radius 1. For any ordered pair, , on the unit circle, and . For example, and .  The unit circle is commonly viewed as being sectioned into four quadrants.    Quadrant I: Top right quadrant; ; and are positive    Quadrant II: Top left quadrant; ; is positive, is negative    Quadrant III: Bottom left quadrant; ; and are negative    Quadrant IV: Bottom right quadrant; ; is negative, is positive      Graphs of trigonometric functions    Inverse trigonometric functions  Trigonometric functions in their conventional sense are not one-to-one since the function values repeat several times. So, in order to define inverse trigonometric functions, we must restrict the domains of our trigonometric functions.          is equivalent to     The domain of is     The range of is             is equivalent to     The domain of is     The range of is             is equivalent to     The domain of is     The range of is         Trigonometric identities   Pythagorian Trigonometric Identities                     Addition\/Subtraction Formulas                          Double-Angle Formulas                 "
+},
+{
+  "id": "polynomials-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#polynomials-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "polynomial "
+},
+{
+  "id": "polynomials-3",
+  "level": "2",
+  "url": "sec-precalcrev.html#polynomials-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "degree leading coefficient "
+},
+{
+  "id": "factoringexpanding-3",
+  "level": "2",
+  "url": "sec-precalcrev.html#factoringexpanding-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Difference of squares Sum of cubes Difference of cubes Perfect square trinomial "
+},
+{
+  "id": "functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "function "
+},
+{
+  "id": "functions-3",
+  "level": "2",
+  "url": "sec-precalcrev.html#functions-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Verbally Numerically Graphically Algebraically "
+},
+{
+  "id": "domain-and-range-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#domain-and-range-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "domain range "
+},
+{
+  "id": "basic-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#basic-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Linear Even Power Odd Power Even Root Odd Root Odd Rational Even Rational Absolute Value "
+},
+{
+  "id": "piecewise-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#piecewise-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "piecewise function "
+},
+{
+  "id": "composite-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#composite-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "composite function "
+},
+{
+  "id": "evenodd-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#evenodd-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "even function "
+},
+{
+  "id": "evenodd-functions-4",
+  "level": "2",
+  "url": "sec-precalcrev.html#evenodd-functions-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "odd function "
+},
+{
+  "id": "inverse-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#inverse-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "inverse function "
+},
+{
+  "id": "exponential-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#exponential-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "exponential function base "
+},
+{
+  "id": "exponential-functions-3",
+  "level": "2",
+  "url": "sec-precalcrev.html#exponential-functions-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "natural exponential function "
+},
+{
+  "id": "logarithmic-functions-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#logarithmic-functions-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "logarithmic function "
+},
+{
+  "id": "logarithmic-functions-3",
+  "level": "2",
+  "url": "sec-precalcrev.html#logarithmic-functions-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "natural logarithmic function "
+},
+{
+  "id": "the-unit-circle-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#the-unit-circle-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "unit circle "
+},
+{
+  "id": "trigonometric-identities-2",
+  "level": "2",
+  "url": "sec-precalcrev.html#trigonometric-identities-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Pythagorian Trigonometric Identities "
+},
+{
+  "id": "trigonometric-identities-4",
+  "level": "2",
+  "url": "sec-precalcrev.html#trigonometric-identities-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Addition\/Subtraction Formulas "
+},
+{
+  "id": "trigonometric-identities-6",
+  "level": "2",
+  "url": "sec-precalcrev.html#trigonometric-identities-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Double-Angle Formulas "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
